74e6c76de2b4f0db82cf281d1b05be9b1caece2f
jianhui <git.fostars.com>
2022-06-24 11:05:17 +0800

    release it

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,7 @@
+
 module.exports = {
   root: true,
   extends: [
-    '@fostars/eslint-config'
+    '@fostars/eslint-config/base'
   ]
 }
--- packages/cli/src/cli.ts
+++ packages/cli/src/cli.ts
@@ -2,6 +2,7 @@ import { Command } from 'commander'
 import { cliVersion } from './index'
 import { build } from './commands/build'
 import { dev } from './commands/dev'
+import { release } from './commands/release'
 
 const program = new Command()
 
@@ -18,5 +19,10 @@ program
   .command('dev')
   .description('development')
   .action(dev)
+
+program
+  .command('release')
+  .description('release version')
+  .action(release)
 
 program.parse()
