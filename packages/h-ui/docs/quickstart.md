8c6f2af0c8d2c066d7cb6131382b4b2a5482570d
jianhui <git.fostars.com>
2022-07-11 01:28:27 +0800

    init ui

---
+++ packages/h-ui/docs/quickstart.md
@@ -0,0 +1,14 @@
+# 快速上手
+
+### 安装
+
+```bash
+# 通过 npm
+npm i h-ui
+
+# 通过 yarn
+yarn add h-ui
+
+# 通过 pnpm
+pnpm add h-ui
+```
---
+++ packages/h-ui/vant.config.js
@@ -0,0 +1,39 @@
+module.exports = {
+  name: 'h-ui',
+  build: {
+    css: {
+      preprocessor: 'less',
+    },
+    site: {
+      publicPath: '/h-ui/',
+    },
+  },
+  site: {
+    title: 'h-ui',
+    logo: 'https://img.yzcdn.cn/vant/logo.png',
+    nav: [
+      {
+        title: '开发指南',
+        items: [
+          {
+            path: 'home',
+            title: '介绍',
+          },
+          {
+            path: 'quickstart',
+            title: '快速上手',
+          },
+        ],
+      },
+      {
+        title: '基础组件',
+        items: [
+          {
+            path: 'demo-button',
+            title: 'DemoButton 按钮',
+          },
+        ],
+      },
+    ],
+  },
+};
